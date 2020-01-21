be4c745e9bb979907855704748eb0a2671de7f98
Andrew Sisley <a.j.sisley@hotmail.com>
2020-01-21 22:34:26 +0100

    Implement CqlReadable for tiny_text

--- cql_storage/src/tiny_text.rs
+++ cql_storage/src/tiny_text.rs
@@ -32,24 +32,26 @@ impl CqlWritable for TinyText {
     }
 }
 
-pub fn read_from_db(db_location: &str, value_location: u64) -> String {
-	let mut file = File::open(&db_location).unwrap();
+impl CqlReadable for TinyText {
+    fn read_from_db(db_location: &str, value_location: u64) -> Self::ValueType {
+        let mut file = File::open(&db_location).unwrap();
 
-	file.seek(SeekFrom::Start(value_location * VALUE_SIZE as u64)).unwrap();
+        file.seek(SeekFrom::Start(value_location * VALUE_SIZE as u64)).unwrap();
 
-    let mut size_buffer = [0; 2];
-    file.read(&mut size_buffer).unwrap();
+        let mut size_buffer = [0; 2];
+        file.read(&mut size_buffer).unwrap();
 
-    let mut size_rdr = Cursor::new(size_buffer);
-    let size = usize::from(size_rdr.read_u16::<LittleEndian>().unwrap());
+        let mut size_rdr = Cursor::new(size_buffer);
+        let size = usize::from(size_rdr.read_u16::<LittleEndian>().unwrap());
 
-    if size == 0 {
-        return String::new()
+        if size == 0 {
+            return String::new()
+        }
+
+        let mut value_buffer = [0; VALUE_SIZE];
+        file.read(&mut value_buffer).unwrap();
+
+        let string_bytes = value_buffer[0..size].to_vec();
+        String::from_utf8(string_bytes).unwrap()
     }
-
-    let mut value_buffer = [0; VALUE_SIZE];
-    file.read(&mut value_buffer).unwrap();
-
-    let string_bytes = value_buffer[0..size].to_vec();
-    String::from_utf8(string_bytes).unwrap()
 }
