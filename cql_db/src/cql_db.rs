44816ebe648c177aeb27707861d2cd91f582cc4a
Andrew Sisley <a.j.sisley@hotmail.com>
2020-02-08 21:49:05 +0000

    Remove comment

--- cql_db/src/cql_db.rs
+++ cql_db/src/cql_db.rs
@@ -112,7 +112,6 @@ fn create_key_library(db_location: &str, x_axis: &AxisDefinition, y_axis: &AxisD
 }
 
 fn grow_database(db_location: &str, size_to_grow: u64, value_size: usize) {
-    // FIXME: Need to make sure that this operation is atomic.
     let file = OpenOptions::new().write(true).open(db_location).unwrap();
     file.set_len(file.metadata().unwrap().len() + size_to_grow * value_size as u64).unwrap();
 }
