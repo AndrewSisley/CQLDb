b4886d4ffc90abdb6c68da883017bf4804acd038
Andrew Sisley <a.j.sisley@hotmail.com>
2020-03-25 18:34:42 +0000

    WIP - add untested maths for calc index

--- cql_db/src/vectors.rs
+++ cql_db/src/vectors.rs
@@ -1,3 +1,25 @@
 pub fn calculate_index(x: u64, y: u64, y_max: u64) -> u64 {
 	((x - 1) * y_max) + (y - 1)
 }
+
+pub fn calculate_index_icosahedral(x: u64, y: u64, m: u64) -> u64 {
+    let mut index = x - 1;
+
+    let m1_bound = m + 1;
+    let y1_max = if y < m1_bound { y } else { m1_bound };
+    for y1 in 2..y1_max {
+        index = index + (y1 - 1);
+    }
+
+    let m2_bound = (m * 2) + 1;
+    let y2_max = if y < m2_bound { y } else { m2_bound };
+    for _y2 in m..y2_max {
+        index = index + m;
+    }
+
+    for y3 in m2_bound..y {
+        index = index + (m - (y3 - m2_bound));
+    }
+
+    index
+}
