8d8865b04a771193ec2f39c425fe1197df21001b
Andrew Sisley <a.j.sisley@hotmail.com>
2020-06-03 20:53:27 +0100

    Add 4d single point read-write test helper

--- cql_storage_types/cql_storage_type_testing_lib/src/cql_storage_type_testing_lib.rs
+++ cql_storage_types/cql_storage_type_testing_lib/src/cql_storage_type_testing_lib.rs
@@ -31,3 +31,38 @@ pub fn _1d_database_allows_for_single_point_read_writes<TStore: CqlWritable + Cq
 
     assert_eq!(result1, value);
 }
+
+pub fn _4d_database_allows_for_single_point_read_writes<TStore: CqlWritable + CqlReadable>(db_location: &str, value: TStore::ValueType)
+        where TStore::ValueType: Copy + Debug + PartialEq {
+    let axis = [
+        2,
+        5,
+        3,
+        2,
+    ];
+
+    let point1 = [2, 4, 3, 1];
+
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &axis
+    ).unwrap();
+
+    cql_db::link_dimensions_unchecked::<TStore>(
+        db_location,
+        &point1[0..3],
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &point1,
+        value
+    ).unwrap();
+
+    let result1 = cql_db::read_value_unchecked::<TStore>(
+        db_location,
+        &point1
+    ).unwrap();
+
+    assert_eq!(result1, value);
+}
