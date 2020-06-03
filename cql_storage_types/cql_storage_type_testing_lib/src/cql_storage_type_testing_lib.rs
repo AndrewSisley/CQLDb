e0699c195689874e7f4745203d6388e38ce8a8c7
Andrew Sisley <a.j.sisley@hotmail.com>
2020-06-03 20:47:01 +0100

    Add 1d single point read-write test helper

--- cql_storage_types/cql_storage_type_testing_lib/src/cql_storage_type_testing_lib.rs
+++ cql_storage_types/cql_storage_type_testing_lib/src/cql_storage_type_testing_lib.rs
@@ -1,1 +1,33 @@
+/*!
+Testing helper library for cql_model storage types.
+*/
+use std::fmt::{ Debug };
+use std::cmp::{ PartialEq };
+use cql_model::{ CqlWritable, CqlReadable };
 
+pub fn _1d_database_allows_for_single_point_read_writes<TStore: CqlWritable + CqlReadable>(db_location: &str, value: TStore::ValueType)
+        where TStore::ValueType: Copy + Debug + PartialEq {
+    let axis = [
+        2,
+    ];
+
+    let point1 = [2];
+
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &axis
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &point1,
+        value
+    ).unwrap();
+
+    let result1 = cql_db::read_value_unchecked::<TStore>(
+        db_location,
+        &point1
+    ).unwrap();
+
+    assert_eq!(result1, value);
+}
