63f27423833f3205feb933534a6eaa0e7ba15e0b
Andrew Sisley <a.j.sisley@hotmail.com>
2020-07-23 13:45:07 +0100

    Add _1d_write_location_100000 benchmark fn

--- cql_storage_types/cql_storage_type_testing_lib/src/benches/write_single.rs
+++ cql_storage_types/cql_storage_type_testing_lib/src/benches/write_single.rs
@@ -20,3 +20,24 @@ pub fn _1d_write_location_1<'a, TStore: CqlWritable>(db_location: &'a str) -> Bo
         ).unwrap();
     })
 }
+
+pub fn _1d_write_location_100000<'a, TStore: CqlWritable>(db_location: &'a str) -> Box<dyn Fn(TStore::ValueType) + 'a> {
+    let axis = [
+        100000,
+    ];
+
+    static POINT1: [u64; 1] = [100000];
+
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &axis
+    ).unwrap();
+
+    Box::new(move |value| -> () {
+        cql_db::write_value_unchecked::<TStore>(
+            db_location,
+            &POINT1,
+            value
+        ).unwrap();
+    })
+}
