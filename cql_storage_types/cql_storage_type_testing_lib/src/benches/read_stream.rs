0ca4c249282fc150a634f63bb3e0a2b15a7aa145
Andrew Sisley <a.j.sisley@hotmail.com>
2020-07-23 16:20:19 +0100

    Add _4d_read_empty_location_1_1_1_1_to_1_1_1_1 benchmark fn

--- cql_storage_types/cql_storage_type_testing_lib/src/benches/read_stream.rs
+++ cql_storage_types/cql_storage_type_testing_lib/src/benches/read_stream.rs
@@ -62,3 +62,39 @@ pub fn _1d_read_empty_location_50000_to_100000<'a, TStore: CqlStreamReadable>(
         unpack_stream(&mut stream, N_VALUES_TO_READ, &mut result);
     }
 }
+
+pub fn _4d_read_empty_location_1_1_1_1_to_1_1_1_1<'a, TStore: CqlStreamReadable>(
+            db_location: &'a str,
+            unpack_stream: &'a dyn Fn(&mut Cursor<Vec<u8>>, usize, &mut [TStore::ValueType])
+        ) -> impl Fn() + 'a
+        where TStore::ValueType: Default {
+    const N_VALUES_TO_READ: usize = 1;
+    static BASE_POINT: [u64; 4] = [1, 1, 1, 1];
+
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &[1, 1, 1, 1]
+    ).unwrap();
+
+    cql_db::link_dimensions_unchecked::<TStore>(
+        db_location,
+        &BASE_POINT[0..3],
+    ).unwrap();
+
+    move || -> () {
+        let mut result = Vec::with_capacity(N_VALUES_TO_READ);
+        result.resize_with(N_VALUES_TO_READ, Default::default);
+        let mut stream = Cursor::new(Vec::new());
+
+        cql_db::read_to_stream_unchecked::<TStore>(
+            db_location,
+            &mut stream,
+            &BASE_POINT,
+            N_VALUES_TO_READ as u64
+        ).unwrap();
+
+        stream.seek(SeekFrom::Start(0)).unwrap();
+
+        unpack_stream(&mut stream, N_VALUES_TO_READ, &mut result);
+    }
+}
