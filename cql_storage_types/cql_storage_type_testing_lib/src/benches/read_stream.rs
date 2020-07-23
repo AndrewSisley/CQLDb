020625524e9a494e4a03fa056b101759212f9a8a
Andrew Sisley <a.j.sisley@hotmail.com>
2020-07-23 17:53:44 +0100

    Add _4d_read_populated_location_1_1_1_50000_to_1_1_1_100000 benchmark fn

--- cql_storage_types/cql_storage_type_testing_lib/src/benches/read_stream.rs
+++ cql_storage_types/cql_storage_type_testing_lib/src/benches/read_stream.rs
@@ -255,3 +255,48 @@ pub fn _4d_read_empty_location_1_1_1_50000_to_1_1_1_100000<'a, TStore: CqlStream
         unpack_stream(&mut stream, N_VALUES_TO_READ, &mut result);
     }
 }
+
+pub fn _4d_read_populated_location_1_1_1_50000_to_1_1_1_100000<'a, TStore: CqlStreamReadable + CqlWritable>(
+            db_location: &'a str,
+            value_generator: &'a dyn Fn(u64) -> TStore::ValueType,
+            unpack_stream: &'a dyn Fn(&mut Cursor<Vec<u8>>, usize, &mut [TStore::ValueType])
+        ) -> impl Fn() + 'a
+        where TStore::ValueType: Default {
+    const N_VALUES_TO_READ: usize = 50000;
+    static BASE_POINT: [u64; 4] = [1, 1, 1, 50000];
+
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &[1, 1, 1, 100000]
+    ).unwrap();
+
+    cql_db::link_dimensions_unchecked::<TStore>(
+        db_location,
+        &BASE_POINT[0..3],
+    ).unwrap();
+
+    for index in 0..N_VALUES_TO_READ {
+        cql_db::write_value_unchecked::<TStore>(
+            db_location,
+            &[1, 1, 1, BASE_POINT[3] + index as u64],
+            value_generator(index as u64)
+        ).unwrap();
+    }
+
+    move || -> () {
+        let mut result = Vec::with_capacity(N_VALUES_TO_READ);
+        result.resize_with(N_VALUES_TO_READ, Default::default);
+        let mut stream = Cursor::new(Vec::new());
+
+        cql_db::read_to_stream_unchecked::<TStore>(
+            db_location,
+            &mut stream,
+            &BASE_POINT,
+            N_VALUES_TO_READ as u64
+        ).unwrap();
+
+        stream.seek(SeekFrom::Start(0)).unwrap();
+
+        unpack_stream(&mut stream, N_VALUES_TO_READ, &mut result);
+    }
+}
