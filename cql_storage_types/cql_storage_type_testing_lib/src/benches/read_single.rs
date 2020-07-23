2a2a036d1dab0a36c1a514a2106200a09a562359
Andrew Sisley <a.j.sisley@hotmail.com>
2020-07-23 11:25:01 +0100

    Add _4d_read_location_1_1_1_1 benchmark fn

--- cql_storage_types/cql_storage_type_testing_lib/src/benches/read_single.rs
+++ cql_storage_types/cql_storage_type_testing_lib/src/benches/read_single.rs
@@ -51,3 +51,37 @@ pub fn _1d_read_location_100000<'a, TStore: CqlWritable + CqlReadable>(db_locati
         ).unwrap();
     }
 }
+
+pub fn _4d_read_location_1_1_1_1<'a, TStore: CqlWritable + CqlReadable>(db_location: &'a str, value: TStore::ValueType) -> impl Fn() + 'a {
+    let axis = [
+        1,
+        1,
+        1,
+        1,
+    ];
+
+    static POINT1: [u64; 4] = [1, 1, 1, 1];
+
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &axis
+    ).unwrap();
+
+    cql_db::link_dimensions_unchecked::<TStore>(
+        db_location,
+        &POINT1[0..3],
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &POINT1,
+        value
+    ).unwrap();
+
+    move || -> () {
+        cql_db::read_value_unchecked::<TStore>(
+            db_location,
+            &POINT1
+        ).unwrap();
+    }
+}
