44dded84e9c77dc9f7f236f1b577dcc7ea6f2214
Andrew Sisley <a.j.sisley@hotmail.com>
2020-09-29 17:51:34 +0100

    Add singlepoint with_overwrite test fns for 1d db

--- cql_storage_types/cql_storage_type_testing_lib/src/tests/single_point_read_writes/_1d_database/with_overwrite/mod.rs
+++ cql_storage_types/cql_storage_type_testing_lib/src/tests/single_point_read_writes/_1d_database/with_overwrite/mod.rs
@@ -2,4 +2,3 @@ mod test_functions;
 pub mod first_index;
 pub mod midpoint;
 pub mod last_index;
-pub mod with_overwrite;
---
+++ cql_storage_types/cql_storage_type_testing_lib/src/tests/single_point_read_writes/_1d_database/with_overwrite/test_functions.rs
@@ -0,0 +1,247 @@
+use std::fmt::{ Debug };
+use std::cmp::{ PartialEq };
+use cql_model::{ CqlWritable, CqlReadable };
+
+const AXIS: [u64; 1] = [
+    3,
+];
+
+pub fn unchecked<TStore: CqlWritable + CqlReadable>(db_location: &str, value1: TStore::ValueType, value2: TStore::ValueType, point: [u64; 1])
+        where TStore::ValueType: Copy + Debug + PartialEq {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &point,
+        value1
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &point,
+        value2
+    ).unwrap();
+
+    let result1 = cql_db::read_value_unchecked::<TStore>(
+        db_location,
+        &point
+    ).unwrap();
+
+    assert_eq!(result1, value2);
+}
+
+pub fn checked<TStore: CqlWritable + CqlReadable>(db_location: &str, value1: TStore::ValueType, value2: TStore::ValueType, point: [u64; 1])
+        where TStore::ValueType: Copy + Debug + PartialEq {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &point,
+        value1
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &point,
+        value2
+    ).unwrap();
+
+    let result1 = cql_db::read_value::<TStore>(
+        db_location,
+        &point
+    ).unwrap();
+
+    assert_eq!(result1, value2);
+}
+
+pub fn unchecked_write_unchecked_overwrite_checked_read<TStore: CqlWritable + CqlReadable>(
+            db_location: &str,
+            value1: TStore::ValueType,
+            value2: TStore::ValueType,
+            point: [u64; 1]
+        ) where TStore::ValueType: Copy + Debug + PartialEq {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &point,
+        value1
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &point,
+        value2
+    ).unwrap();
+
+    let result1 = cql_db::read_value::<TStore>(
+        db_location,
+        &point
+    ).unwrap();
+
+    assert_eq!(result1, value2);
+}
+
+pub fn unchecked_write_checked_overwrite_checked_read<TStore: CqlWritable + CqlReadable>(
+            db_location: &str,
+            value1: TStore::ValueType,
+            value2: TStore::ValueType,
+            point: [u64; 1]
+        ) where TStore::ValueType: Copy + Debug + PartialEq {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &point,
+        value1
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &point,
+        value2
+    ).unwrap();
+
+    let result1 = cql_db::read_value::<TStore>(
+        db_location,
+        &point
+    ).unwrap();
+
+    assert_eq!(result1, value2);
+}
+
+pub fn checked_write_unchecked_overwrite_checked_read<TStore: CqlWritable + CqlReadable>(
+            db_location: &str,
+            value1: TStore::ValueType,
+            value2: TStore::ValueType,
+            point: [u64; 1]
+        ) where TStore::ValueType: Copy + Debug + PartialEq {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &point,
+        value1
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &point,
+        value2
+    ).unwrap();
+
+    let result1 = cql_db::read_value::<TStore>(
+        db_location,
+        &point
+    ).unwrap();
+
+    assert_eq!(result1, value2);
+}
+
+pub fn checked_write_unchecked_overwrite_unchecked_read<TStore: CqlWritable + CqlReadable>(
+            db_location: &str,
+            value1: TStore::ValueType,
+            value2: TStore::ValueType,
+            point: [u64; 1]
+        ) where TStore::ValueType: Copy + Debug + PartialEq {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &point,
+        value1
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &point,
+        value2
+    ).unwrap();
+
+    let result1 = cql_db::read_value_unchecked::<TStore>(
+        db_location,
+        &point
+    ).unwrap();
+
+    assert_eq!(result1, value2);
+}
+
+pub fn unchecked_write_checked_overwrite_unchecked_read<TStore: CqlWritable + CqlReadable>(
+            db_location: &str,
+            value1: TStore::ValueType,
+            value2: TStore::ValueType,
+            point: [u64; 1]
+        ) where TStore::ValueType: Copy + Debug + PartialEq {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &point,
+        value1
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &point,
+        value2
+    ).unwrap();
+
+    let result1 = cql_db::read_value_unchecked::<TStore>(
+        db_location,
+        &point
+    ).unwrap();
+
+    assert_eq!(result1, value2);
+}
+
+pub fn checked_write_checked_overwrite_unchecked_read<TStore: CqlWritable + CqlReadable>(
+            db_location: &str,
+            value1: TStore::ValueType,
+            value2: TStore::ValueType,
+            point: [u64; 1]
+        ) where TStore::ValueType: Copy + Debug + PartialEq {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &point,
+        value1
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &point,
+        value2
+    ).unwrap();
+
+    let result1 = cql_db::read_value_unchecked::<TStore>(
+        db_location,
+        &point
+    ).unwrap();
+
+    assert_eq!(result1, value2);
+}
