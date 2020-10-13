34549932f8e0982af68adb6bbf9d68508846739e
Andrew Sisley <a.j.sisley@hotmail.com>
2020-10-13 19:18:04 +0100

    Add singlepoint i16 with_overwrite tests for 4d db

--- cql_storage_types/cql_i16/tests/i16.rs
+++ cql_storage_types/cql_i16/tests/i16.rs
@@ -4,20 +4,8 @@ use serial_test::serial;
 use std::io::{ Cursor };
 use constants::DATABASE_LOCATION;
 use cql_i16::{ I16, unpack_stream };
-use cql_storage_type_testing_lib::tests::{ read_write_single_unchecked, read_write_stream_unchecked };
+use cql_storage_type_testing_lib::tests::{ read_write_stream_unchecked };
 pub mod single_point_read_writes;
-
-#[test]
-#[serial]
-fn _4d_i16_database_allows_for_single_point_read_writes_given_multiple_values_and_overwrites() {
-    read_write_single_unchecked::_4d_database_allows_for_single_point_read_writes_given_multiple_values_and_overwrites::<I16>(
-        DATABASE_LOCATION,
-        5,
-        -20,
-        0,
-        30000
-    );
-}
 
 #[test]
 #[serial]
