165c87002c708198fbb6dd0cfb9e8668a48443be
Andrew Sisley <a.j.sisley@hotmail.com>
2020-10-29 18:45:05 +0000

    Add stream read point write test fns for 4d

---
+++ cql_storage_types/cql_storage_type_testing_lib/src/tests/stream_read_point_writes/_4d_database/first_index.rs
@@ -0,0 +1,43 @@
+use std::fmt::{ Debug };
+use std::cmp::{ PartialEq };
+use std::io::{ Cursor };
+use cql_model::{ CqlWritable, CqlStreamReadable };
+use crate::tests::stream_read_point_writes::_4d_database::test_functions;
+
+const POINT: [u64; 4] = [1, 1, 1, 1];
+
+pub fn unchecked<'a, TStore: CqlWritable + CqlStreamReadable>(
+            db_location: &str,
+            value: TStore::ValueType,
+            unpack_stream: &'a dyn Fn(&mut Cursor<Vec<u8>>, usize, &mut [TStore::ValueType])
+        )
+        where TStore::ValueType: Copy + Debug + PartialEq + Default {
+    test_functions::unchecked::<TStore>(db_location, value, POINT, unpack_stream)
+}
+
+pub fn checked<'a, TStore: CqlWritable + CqlStreamReadable>(
+            db_location: &str,
+            value: TStore::ValueType,
+            unpack_stream: &'a dyn Fn(&mut Cursor<Vec<u8>>, usize, &mut [TStore::ValueType])
+        )
+        where TStore::ValueType: Copy + Debug + PartialEq + Default {
+    test_functions::checked::<TStore>(db_location, value, POINT, unpack_stream)
+}
+
+pub fn unchecked_write_checked_read<'a, TStore: CqlWritable + CqlStreamReadable>(
+            db_location: &str,
+            value: TStore::ValueType,
+            unpack_stream: &'a dyn Fn(&mut Cursor<Vec<u8>>, usize, &mut [TStore::ValueType])
+        )
+        where TStore::ValueType: Copy + Debug + PartialEq + Default {
+    test_functions::unchecked_write_checked_read::<TStore>(db_location, value, POINT, unpack_stream)
+}
+
+pub fn checked_write_unchecked_read<'a, TStore: CqlWritable + CqlStreamReadable>(
+            db_location: &str,
+            value: TStore::ValueType,
+            unpack_stream: &'a dyn Fn(&mut Cursor<Vec<u8>>, usize, &mut [TStore::ValueType])
+        )
+        where TStore::ValueType: Copy + Debug + PartialEq + Default {
+    test_functions::checked_write_unchecked_read::<TStore>(db_location, value, POINT, unpack_stream)
+}
---
+++ cql_storage_types/cql_storage_type_testing_lib/src/tests/stream_read_point_writes/_4d_database/full.rs
@@ -0,0 +1,240 @@
+use std::fmt::{ Debug };
+use std::cmp::{ PartialEq };
+use std::io::{ Cursor, SeekFrom, Seek };
+use cql_model::{ CqlWritable, CqlStreamReadable };
+
+const AXIS: [u64; 4] = [
+    3,
+    5,
+    7,
+    9,
+];
+
+const N_VALUES_TO_READ: usize = 9;
+const POINT1: [u64; 4] = [2, 3, 4, 1];
+const POINT2: [u64; 4] = [2, 3, 4, 5];
+const POINT3: [u64; 4] = [2, 3, 4, 9];
+
+pub fn unchecked<'a, TStore: CqlWritable + CqlStreamReadable>(
+            db_location: &str,
+            value1: TStore::ValueType,
+            value2: TStore::ValueType,
+            value3: TStore::ValueType,
+            unpack_stream: &'a dyn Fn(&mut Cursor<Vec<u8>>, usize, &mut [TStore::ValueType])
+        )
+        where TStore::ValueType: Copy + Debug + PartialEq + Default {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::link_dimensions_unchecked::<TStore>(
+        db_location,
+        &POINT1[0..3],
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &POINT1,
+        value1
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &POINT2,
+        value2
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &POINT3,
+        value3
+    ).unwrap();
+
+    let mut result = Vec::with_capacity(N_VALUES_TO_READ);
+    result.resize_with(N_VALUES_TO_READ, Default::default);
+    let mut stream = Cursor::new(Vec::new());
+
+    cql_db::read_to_stream_unchecked::<TStore>(
+        db_location,
+        &mut stream,
+        &POINT1,
+        N_VALUES_TO_READ as u64
+    ).unwrap();
+
+    stream.seek(SeekFrom::Start(0)).unwrap();
+
+    unpack_stream(&mut stream, N_VALUES_TO_READ, &mut result);
+
+    assert_eq!(result[0], value1);
+    assert_eq!(result[4], value2);
+    assert_eq!(result[8], value3);
+}
+
+pub fn checked<'a, TStore: CqlWritable + CqlStreamReadable>(
+            db_location: &str,
+            value1: TStore::ValueType,
+            value2: TStore::ValueType,
+            value3: TStore::ValueType,
+            unpack_stream: &'a dyn Fn(&mut Cursor<Vec<u8>>, usize, &mut [TStore::ValueType])
+        )
+        where TStore::ValueType: Copy + Debug + PartialEq + Default {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::link_dimensions_unchecked::<TStore>(
+        db_location,
+        &POINT1[0..3],
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &POINT1,
+        value1
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &POINT2,
+        value2
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &POINT3,
+        value3
+    ).unwrap();
+
+    let mut result = Vec::with_capacity(N_VALUES_TO_READ);
+    result.resize_with(N_VALUES_TO_READ, Default::default);
+    let mut stream = Cursor::new(Vec::new());
+
+    cql_db::read_to_stream::<TStore>(
+        db_location,
+        &mut stream,
+        &POINT1,
+        N_VALUES_TO_READ as u64
+    ).unwrap();
+
+    stream.seek(SeekFrom::Start(0)).unwrap();
+
+    unpack_stream(&mut stream, N_VALUES_TO_READ, &mut result);
+
+    assert_eq!(result[0], value1);
+    assert_eq!(result[4], value2);
+    assert_eq!(result[8], value3);
+}
+
+pub fn unchecked_write_checked_read<'a, TStore: CqlWritable + CqlStreamReadable>(
+            db_location: &str,
+            value1: TStore::ValueType,
+            value2: TStore::ValueType,
+            value3: TStore::ValueType,
+            unpack_stream: &'a dyn Fn(&mut Cursor<Vec<u8>>, usize, &mut [TStore::ValueType])
+        )
+        where TStore::ValueType: Copy + Debug + PartialEq + Default {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::link_dimensions_unchecked::<TStore>(
+        db_location,
+        &POINT1[0..3],
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &POINT1,
+        value1
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &POINT2,
+        value2
+    ).unwrap();
+
+    cql_db::write_value_unchecked::<TStore>(
+        db_location,
+        &POINT3,
+        value3
+    ).unwrap();
+
+    let mut result = Vec::with_capacity(N_VALUES_TO_READ);
+    result.resize_with(N_VALUES_TO_READ, Default::default);
+    let mut stream = Cursor::new(Vec::new());
+
+    cql_db::read_to_stream::<TStore>(
+        db_location,
+        &mut stream,
+        &POINT1,
+        N_VALUES_TO_READ as u64
+    ).unwrap();
+
+    stream.seek(SeekFrom::Start(0)).unwrap();
+
+    unpack_stream(&mut stream, N_VALUES_TO_READ, &mut result);
+
+    assert_eq!(result[0], value1);
+    assert_eq!(result[4], value2);
+    assert_eq!(result[8], value3);
+}
+
+pub fn checked_write_unchecked_read<'a, TStore: CqlWritable + CqlStreamReadable>(
+            db_location: &str,
+            value1: TStore::ValueType,
+            value2: TStore::ValueType,
+            value3: TStore::ValueType,
+            unpack_stream: &'a dyn Fn(&mut Cursor<Vec<u8>>, usize, &mut [TStore::ValueType])
+        )
+        where TStore::ValueType: Copy + Debug + PartialEq + Default {
+    cql_db::create_db_unchecked::<TStore>(
+        db_location,
+        &AXIS
+    ).unwrap();
+
+    cql_db::link_dimensions_unchecked::<TStore>(
+        db_location,
+        &POINT1[0..3],
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &POINT1,
+        value1
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &POINT2,
+        value2
+    ).unwrap();
+
+    cql_db::write_value::<TStore>(
+        db_location,
+        &POINT3,
+        value3
+    ).unwrap();
+
+    let mut result = Vec::with_capacity(N_VALUES_TO_READ);
+    result.resize_with(N_VALUES_TO_READ, Default::default);
+    let mut stream = Cursor::new(Vec::new());
+
+    cql_db::read_to_stream_unchecked::<TStore>(
+        db_location,
+        &mut stream,
+        &POINT1,
+        N_VALUES_TO_READ as u64
+    ).unwrap();
+
+    stream.seek(SeekFrom::Start(0)).unwrap();
+
+    unpack_stream(&mut stream, N_VALUES_TO_READ, &mut result);
+
+    assert_eq!(result[0], value1);
+    assert_eq!(result[4], value2);
+    assert_eq!(result[8], value3);
+}
