c440ddaa3b7bd97da0cf94780a4535185eed1b01
Andrew Sisley <a.j.sisley@hotmail.com>
2020-11-01 16:35:20 +0000

    Add stream read point write overwrite tests for 4d i16

---
+++ cql_storage_types/cql_i16/tests/stream_read_point_writes/_4d_database/with_overwrite/midpoint.rs
@@ -0,0 +1,93 @@
+use serial_test::serial;
+use crate::constants::DATABASE_LOCATION;
+use cql_i16::{ I16 };
+use cql_storage_type_testing_lib::tests::stream_read_point_writes::_4d_database::with_overwrite::midpoint;
+use crate::unpack_i16_stream;
+
+#[test]
+#[serial]
+fn unchecked() {
+    midpoint::unchecked::<I16>(
+        DATABASE_LOCATION,
+        42,
+        15,
+        &unpack_i16_stream
+    );
+}
+
+#[test]
+#[serial]
+fn checked() {
+    midpoint::checked::<I16>(
+        DATABASE_LOCATION,
+        42,
+        15,
+        &unpack_i16_stream
+    );
+}
+
+#[test]
+#[serial]
+fn unchecked_write_unchecked_overwrite_checked_read() {
+    midpoint::unchecked_write_unchecked_overwrite_checked_read::<I16>(
+        DATABASE_LOCATION,
+        42,
+        15,
+        &unpack_i16_stream
+    );
+}
+
+#[test]
+#[serial]
+fn unchecked_write_checked_overwrite_checked_read() {
+    midpoint::unchecked_write_checked_overwrite_checked_read::<I16>(
+        DATABASE_LOCATION,
+        42,
+        15,
+        &unpack_i16_stream
+    );
+}
+
+#[test]
+#[serial]
+fn checked_write_unchecked_overwrite_checked_read() {
+    midpoint::checked_write_unchecked_overwrite_checked_read::<I16>(
+        DATABASE_LOCATION,
+        42,
+        15,
+        &unpack_i16_stream
+    );
+}
+
+#[test]
+#[serial]
+fn checked_write_unchecked_overwrite_unchecked_read() {
+    midpoint::checked_write_unchecked_overwrite_unchecked_read::<I16>(
+        DATABASE_LOCATION,
+        42,
+        15,
+        &unpack_i16_stream
+    );
+}
+
+#[test]
+#[serial]
+fn unchecked_write_checked_overwrite_unchecked_read() {
+    midpoint::unchecked_write_checked_overwrite_unchecked_read::<I16>(
+        DATABASE_LOCATION,
+        42,
+        15,
+        &unpack_i16_stream
+    );
+}
+
+#[test]
+#[serial]
+fn checked_write_checked_overwrite_unchecked_read() {
+    midpoint::checked_write_checked_overwrite_unchecked_read::<I16>(
+        DATABASE_LOCATION,
+        42,
+        15,
+        &unpack_i16_stream
+    );
+}
