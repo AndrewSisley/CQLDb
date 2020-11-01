b905800da4192f2cfbc8b82ce84de859aafb5d42
Andrew Sisley <a.j.sisley@hotmail.com>
2020-11-01 16:38:04 +0000

    Remove old 4d i16 stream read test

--- cql_storage_types/cql_i16/tests/i16.rs
+++ cql_storage_types/cql_i16/tests/i16.rs
@@ -1,24 +1,9 @@
 mod constants;
 
-use serial_test::serial;
 use std::io::{ Cursor };
-use constants::DATABASE_LOCATION;
-use cql_i16::{ I16, unpack_stream };
-use cql_storage_type_testing_lib::tests::{ read_write_stream_unchecked };
+use cql_i16::{ unpack_stream };
 pub mod single_point_read_writes;
 pub mod stream_read_point_writes;
-
-#[test]
-#[serial]
-fn _4d_i16_database_allows_for_stream_reads() {
-    read_write_stream_unchecked::_4d_database_allows_for_stream_reads::<I16, &dyn Fn(&mut Cursor<Vec<u8>>, usize, &mut [i16])>(
-        DATABASE_LOCATION,
-        42,
-        16,
-        80,
-        &unpack_i16_stream
-    );
-}
 
 fn unpack_i16_stream (stream: &mut Cursor<Vec<u8>>, n_values: usize, result: &mut [i16]) {
     unpack_stream(stream, n_values, |idx, value| {
