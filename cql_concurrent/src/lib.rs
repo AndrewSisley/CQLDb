3d6a4cfa4af7198baed6fb94a7fe24be266a4767
Andrew Sisley <a.j.sisley@hotmail.com>
2021-10-04 09:00:02 -0400

    WIP - more wip writable stuff

--- cql_concurrent/src/lib.rs
+++ cql_concurrent/src/lib.rs
@@ -9,16 +9,26 @@ use cql_model::{
     CqlStreamReadable*/
 };
 use cql_db;
+use cql_db::positional::{
+    calculate_position,
+    write_value_to_position
+};
 
 
+#[derive(Clone)]
 pub struct Database {
-    pub resolution: usize,
-    pub thread_bins: Vec<Arc::<RwLock::<()>>>,
+    thread_bins: Arc::<Vec<RwLock::<()>>>,
+}
+
+impl Database {
+    pub fn new() -> Self {
+        Database {
+            thread_bins: Arc::new(vec![RwLock::new(()), RwLock::new(())])// todo: do properly...
+        }
+    }
 }
 
 pub trait CqlAsyncAccessor {
-    //fn get_thread_discriminator(& self, position: u64) -> usize;// is a terrible interface
-
     fn get_write_lock(& self, position: u64) -> LockResult<RwLockWriteGuard<'_, ()>>;
 }
 
@@ -29,19 +39,19 @@ pub async fn write_value_unchecked<TAccessor: CqlAsyncAccessor, TStore: CqlWrita
             location: &[u64],
             value: TStore::ValueType
         ) -> io::Result<()> {//todo error type etc
-    let position: u64 = 1234; // calculate_position - expose? feels a tad internal...
+    let position = calculate_position(db_location, location)?;// - expose? feels a tad internal...
 
     {
         let _lock = accessor.get_write_lock(position).unwrap();// can still deadlock if multiple read locks in same context I think - see doc, lock this down
-        cql_db::write_value_unchecked::<TStore>(&db_location, location, value).unwrap();//temp
-        //cql_db::write_value_unchecked::<TStore>(&db.location, position, value) // feed position
+        //cql_db::write_value_unchecked::<TStore>(&db_location, location, value).unwrap();//temp
+        write_value_to_position::<TStore>(db_location, position, value)?; // feed position
     }
     Ok::<(), io::Error>(())
 }
 
 impl CqlAsyncAccessor for Database {
     fn get_write_lock(& self, position: u64) -> LockResult<RwLockWriteGuard<'_, ()>> {
-        let thread_discriminator = (position % self.thread_bins.len() as u64) as usize; // temp cast - be smarter when sorting out resolution
+        let thread_discriminator = (position % self.thread_bins.len() as u64) as usize; // temp cast - be smarter when sorting out resolution, this currently will be terrible for stream reads
         self.thread_bins[thread_discriminator].write()
     }
 }
--- cql_concurrent/tests/cql_concurrent.rs
+++ cql_concurrent/tests/cql_concurrent.rs
@@ -6,6 +6,7 @@ use cql_u64::{ U64, unpack_stream };
 use serial_test::serial;
 use futures::executor::{block_on, };
 use futures;
+use std::thread;
 use std::sync::{Arc, RwLock, LockResult, /*RwLockReadGuard,*/ RwLockWriteGuard};
 
 const AXIS: [u64; 1] = [
@@ -15,10 +16,7 @@ const AXIS: [u64; 1] = [
 #[test]
 #[serial]
 fn unchecked() {
-    let db = lib::Database {
-        resolution: u16::MAX as usize,
-        thread_bins: vec![Arc::new(RwLock::new(()))]
-    };
+    let db = lib::Database::new();
 
     cql_db::create_db_unchecked::<U64>(
         DATABASE_LOCATION,
@@ -43,40 +41,80 @@ fn unchecked() {
 #[test]
 #[serial]
 fn unchecked2() {
-    let db = lib::Database {
+    /*let db = lib::Database {
         resolution: u16::MAX as usize,
         thread_bins: vec![Arc::new(RwLock::new(())), Arc::new(RwLock::new(()))]
-    };
+    };*/
+    let db = lib::Database::new();
 
     cql_db::create_db_unchecked::<U64>(
         DATABASE_LOCATION,
         &AXIS
     ).unwrap();
 
-    let f1 = lib::write_value_unchecked::<lib::Database, U64>(// fire these off in their own threads... test is just running sync atm
-        DATABASE_LOCATION,
-        &db,
-        &[5],
-        1
-    );
     
-    let f2 = lib::write_value_unchecked::<lib::Database, U64>(
-        DATABASE_LOCATION,
-        &db,
-        &[5],
-        2
-    );
-    
-    let f3 = lib::write_value_unchecked::<lib::Database, U64>(
-        DATABASE_LOCATION,
-        &db,
-        &[5],
-        3
-    );
 
-    block_on(async {// todo...
-        futures::join!(f1, f2, f3);
+    let db1 = db.clone();
+    let t1 = thread::spawn(move || {
+        let f1 = lib::write_value_unchecked::<lib::Database, U64>(
+            DATABASE_LOCATION,
+            &db1,
+            &[5],
+            1
+        );
+        
+        let f2 = lib::write_value_unchecked::<lib::Database, U64>(
+            DATABASE_LOCATION,
+            &db1,
+            &[5],
+            2
+        );
+        
+        let f3 = lib::write_value_unchecked::<lib::Database, U64>(
+            DATABASE_LOCATION,
+            &db1,
+            &[5],
+            3
+        );
+
+        block_on(async {// todo...
+            futures::join!(f1, f2, f3);
+        });
     });
+
+
+    let db2 = db.clone();
+    let t2 = thread::spawn(move || {
+        let f1 = lib::write_value_unchecked::<lib::Database, U64>(
+            DATABASE_LOCATION,
+            &db2,
+            &[5],
+            1
+        );
+        
+        let f2 = lib::write_value_unchecked::<lib::Database, U64>(
+            DATABASE_LOCATION,
+            &db2,
+            &[5],
+            2
+        );
+        
+        let f3 = lib::write_value_unchecked::<lib::Database, U64>(
+            DATABASE_LOCATION,
+            &db2,
+            &[5],
+            3
+        );
+
+        block_on(async {// todo...
+            futures::join!(f1, f2, f3);
+        });
+    });
+
+    t1.join();
+    t2.join();
+
+    
 
     let result1 = cql_db::read_value_unchecked::<U64>(
         DATABASE_LOCATION,
